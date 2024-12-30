56535acb5b63de7fd401f1eb20922b60e614db7a
Luana Silva <luana.dossantos@zup.com.br>
2024-12-30 11:02:58 -0300

    fix: faz validação do nome na edição de vídeos

--- src/FileHandler/FileHandler.java
+++ src/FileHandler/FileHandler.java
@@ -173,248 +173,252 @@ public class FileHandler {
             System.out.println("Nenhum vídeo encontrado com o título especificado.");
 
 
-        }
 
-        Video videoParaEditar = resultadosEdicao.get(0);
-        Video videoAnteriorParaDeletar = resultadosEdicao.get(0);
+        } else if (!resultadosEdicao.isEmpty()) {
+            Video videoParaEditar = resultadosEdicao.get(0);
+            Video videoAnteriorParaDeletar = resultadosEdicao.get(0);
 
-        System.out.println("Vídeo encontrado:");
-        System.out.println(videoParaEditar);
-        System.out.println("------------------------------");
-
-        // Editar campos
-
-        String resposta;
-
-        do {
-            try {
-                System.out.print("Deseja alterar o título? (S/N): ");
-                resposta = scanner.nextLine().toUpperCase();
-
-                if (!resposta.equals("N") && !resposta.equals("S")) {
-                    System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
-                    System.out.println(" ");
-                    continue;
-                }
-
-                break;
-            } catch (Exception e) {
-                System.out.println(" -- Por favor, tente novamente! --");
-                System.out.println(" ");
-            }
-
-        } while (true);
+            System.out.println("Vídeo encontrado:");
+            System.out.println(videoParaEditar);
+            System.out.println("------------------------------");
 
 
-        if (resposta.equalsIgnoreCase("S")) {
+            // Editar campos
+
+            String resposta;
+
             do {
                 try {
-                    System.out.print("Novo título: ");
-                    String novoTitulo = scanner.nextLine();
+                    System.out.print("Deseja alterar o título? (S/N): ");
+                    resposta = scanner.nextLine().toUpperCase();
 
-                    if (novoTitulo.isBlank()) {
-                        System.out.println(" --- Por favor, escreva o título do vídeo! --- ");
+                    if (!resposta.equals("N") && !resposta.equals("S")) {
+                        System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
                         System.out.println(" ");
                         continue;
                     }
 
-
-                    videoParaEditar = new Video(novoTitulo, videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
-                    break;
-
-                } catch (Exception e) {
-                    System.out.println(" -- Título está em branco! Digite um título. -- ");
-                    System.out.println(" ");
-                    continue;
-                }
-            } while (true);
-
-        }
-
-
-        do {
-            try {
-                System.out.print("Deseja alterar a descrição? (S/N): ");
-                resposta = scanner.nextLine().toUpperCase();
-
-                if (!resposta.equals("N") && !resposta.equals("S")) {
-                    System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
-                    System.out.println(" ");
-                    continue;
-                }
-                break;
-            } catch (Exception e) {
-                System.out.println(" -- Por favor, tente novamente! -- ");
-            }
-        } while (true);
-
-        if (resposta.equalsIgnoreCase("S")) {
-            do {
-                try {
-                    System.out.print("Nova descrição: ");
-                    String novaDescricao = scanner.nextLine();
-
-                    if (novaDescricao.isBlank()) {
-                        System.out.println(" --- Por favor, escreva uma descrição para o vídeo! --- ");
-                        System.out.println(" ");
-                        continue;
-                    }
-
-                    videoParaEditar = new Video(videoParaEditar.getTitulo(), novaDescricao, videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
                     break;
                 } catch (Exception e) {
                     System.out.println(" -- Por favor, tente novamente! --");
                     System.out.println(" ");
-                    continue;
                 }
 
             } while (true);
 
-        }
 
-        do {
-            try {
-                System.out.print("Deseja alterar a duração? (S/N): ");
-                resposta = scanner.nextLine().toUpperCase();
+            if (resposta.equalsIgnoreCase("S")) {
+                do {
+                    try {
+                        System.out.print("Novo título: ");
+                        String novoTitulo = scanner.nextLine();
 
-                if (!resposta.equals("S") && !resposta.equals("N")) {
-                    System.out.println(" --- Por favor, digite apenas 'S' para Sim ou 'N' para Não! --- ");
-                    System.out.println(" ");
-                    continue;
-                }
-                break;
-
-            } catch (Exception e) {
-                System.out.println(" -- Ocorreu um erro! Tente novamente! -- ");
-            }
-        } while (true);
+                        if (novoTitulo.isBlank()) {
+                            System.out.println(" --- Por favor, escreva o título do vídeo! --- ");
+                            System.out.println(" ");
+                            continue;
+                        }
 
 
-        if (resposta.equalsIgnoreCase("S")) {
+                        videoParaEditar = new Video(novoTitulo, videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
+                        break;
 
-            do {
-                try {
-
-                    System.out.print("Nova duração (em minutos): ");
-                    int novaDuracao = scanner.nextInt();
-
-                    if (novaDuracao <= 0) {
-                        System.out.println(" --- Por favor, digite um valor acima de 0! --- ");
+                    } catch (Exception e) {
+                        System.out.println(" -- Título está em branco! Digite um título. -- ");
                         System.out.println(" ");
-                        continue; // Volta ao início do loop para pedir novamente
+                        continue;
                     }
+                } while (true);
 
-                    scanner.nextLine(); // Consumir quebra de linha
-                    videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), novaDuracao, videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
-                    break;
-
-                } catch (Exception e) {
-                    System.out.println(" -- Por favor, digite apenas números! -- ");
-                }
-            } while (true);
-        }
-
-        do {
-            try {
-                System.out.print("Deseja alterar a categoria? (S/N): ");
-                resposta = scanner.nextLine().toUpperCase();
-
-                if (!resposta.equals("S") && !resposta.equals("N")) {
-                    System.out.println(" --- Por favor, digite apenas 'S' para Sim ou 'N' para Não! --- ");
-                    System.out.println(" ");
-                    continue;
-                }
-                break;
-
-            } catch (Exception e) {
-                System.out.println(" -- Ocorreu um erro, tente novamente! -- ");
             }
-        } while (true);
 
 
-        if (resposta.equalsIgnoreCase("S")) {
-
-            int novaCategoriaEscolha;
             do {
                 try {
-                    System.out.println("Selecione a nova categoria:");
-                    System.out.println("1 - Filme");
-                    System.out.println("2 - Série");
-                    System.out.println("3 - Documentário");
-                    System.out.println("4 - Curta-metragem");
-                    System.out.println("5 - Desenho");
-                    System.out.println("6 - Anime");
+                    System.out.print("Deseja alterar a descrição? (S/N): ");
+                    resposta = scanner.nextLine().toUpperCase();
 
-                    novaCategoriaEscolha = scanner.nextInt();
+                    if (!resposta.equals("N") && !resposta.equals("S")) {
+                        System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
+                        System.out.println(" ");
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println(" -- Por favor, tente novamente! -- ");
+                }
+            } while (true);
 
-                    if (novaCategoriaEscolha > 6 || novaCategoriaEscolha < 1) {
-                        System.out.println(" -- Por favor, apenas números de 1 a 6! -- ");
+            if (resposta.equalsIgnoreCase("S")) {
+                do {
+                    try {
+                        System.out.print("Nova descrição: ");
+                        String novaDescricao = scanner.nextLine();
+
+                        if (novaDescricao.isBlank()) {
+                            System.out.println(" --- Por favor, escreva uma descrição para o vídeo! --- ");
+                            System.out.println(" ");
+                            continue;
+                        }
+
+                        videoParaEditar = new Video(videoParaEditar.getTitulo(), novaDescricao, videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" -- Por favor, tente novamente! --");
+                        System.out.println(" ");
                         continue;
                     }
 
+                } while (true);
 
+            }
+
+            do {
+                try {
+                    System.out.print("Deseja alterar a duração? (S/N): ");
+                    resposta = scanner.nextLine().toUpperCase();
+
+                    if (!resposta.equals("S") && !resposta.equals("N")) {
+                        System.out.println(" --- Por favor, digite apenas 'S' para Sim ou 'N' para Não! --- ");
+                        System.out.println(" ");
+                        continue;
+                    }
                     break;
-                } catch (Exception e) {
-                    System.out.println(" -- Por favor, digite apenas números! -- ");
-                    System.out.println(" ");
-                    scanner.nextLine();
-                }
 
+                } catch (Exception e) {
+                    System.out.println(" -- Ocorreu um erro! Tente novamente! -- ");
+                }
             } while (true);
 
 
-            scanner.nextLine(); // Consumir quebra de linha
-            String novaCategoria = switch (novaCategoriaEscolha) {
-                case 1 -> "Filme";
-                case 2 -> "Série";
-                case 3 -> "Documentário";
-                case 4 -> "Curta-metragem";
-                case 5 -> "Desenho";
-                case 6 -> "Anime";
-                default -> videoParaEditar.getCategoria();
-            };
-            videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), novaCategoria, videoParaEditar.getDataPublicacao());
-        }
+            if (resposta.equalsIgnoreCase("S")) {
 
+                do {
+                    try {
 
-        do {
-            try {
-                System.out.print("Deseja alterar a data de publicação? (S/N): ");
-                resposta = scanner.nextLine().toUpperCase();
+                        System.out.print("Nova duração (em minutos): ");
+                        int novaDuracao = scanner.nextInt();
 
-                if (!resposta.equals("N") && !resposta.equals("S")) {
-                    System.out.println(" ");
-                    System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
-                    System.out.println(" ");
-                    continue;
+                        if (novaDuracao <= 0) {
+                            System.out.println(" --- Por favor, digite um valor acima de 0! --- ");
+                            System.out.println(" ");
+                            continue; // Volta ao início do loop para pedir novamente
+                        }
+
+                        scanner.nextLine(); // Consumir quebra de linha
+                        videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), novaDuracao, videoParaEditar.getCategoria(), videoParaEditar.getDataPublicacao());
+                        break;
+
+                    } catch (Exception e) {
+                        System.out.println(" -- Por favor, digite apenas números! -- ");
+                    }
+                } while (true);
+            }
+
+            do {
+                try {
+                    System.out.print("Deseja alterar a categoria? (S/N): ");
+                    resposta = scanner.nextLine().toUpperCase();
+
+                    if (!resposta.equals("S") && !resposta.equals("N")) {
+                        System.out.println(" --- Por favor, digite apenas 'S' para Sim ou 'N' para Não! --- ");
+                        System.out.println(" ");
+                        continue;
+                    }
+                    break;
+
+                } catch (Exception e) {
+                    System.out.println(" -- Ocorreu um erro, tente novamente! -- ");
                 }
-                break;
+            } while (true);
 
 
-            } catch (Exception e) {
-                System.out.println(" -- Houve um erro! Tente novamente! -- ");
-                System.out.println(" ");
+            if (resposta.equalsIgnoreCase("S")) {
 
+                int novaCategoriaEscolha;
+                do {
+                    try {
+                        System.out.println("Selecione a nova categoria:");
+                        System.out.println("1 - Filme");
+                        System.out.println("2 - Série");
+                        System.out.println("3 - Documentário");
+                        System.out.println("4 - Curta-metragem");
+                        System.out.println("5 - Desenho");
+                        System.out.println("6 - Anime");
+
+                        novaCategoriaEscolha = scanner.nextInt();
+
+                        if (novaCategoriaEscolha > 6 || novaCategoriaEscolha < 1) {
+                            System.out.println(" -- Por favor, apenas números de 1 a 6! -- ");
+                            continue;
+                        }
+
+
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" -- Por favor, digite apenas números! -- ");
+                        System.out.println(" ");
+                        scanner.nextLine();
+                    }
+
+                } while (true);
+
+
+                scanner.nextLine(); // Consumir quebra de linha
+                String novaCategoria = switch (novaCategoriaEscolha) {
+                    case 1 -> "Filme";
+                    case 2 -> "Série";
+                    case 3 -> "Documentário";
+                    case 4 -> "Curta-metragem";
+                    case 5 -> "Desenho";
+                    case 6 -> "Anime";
+                    default -> videoParaEditar.getCategoria();
+                };
+                videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), novaCategoria, videoParaEditar.getDataPublicacao());
             }
-        }while (true);
 
 
-        if (resposta.equalsIgnoreCase("S")) {
-            System.out.print("Nova data de publicação (dd/MM/yyyy): ");
-            String novaDataStr = scanner.nextLine();
-            try {
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                Date novaDataPublicacao = sdf.parse(novaDataStr);
-                videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), novaDataPublicacao);
-            } catch (Exception e) {
-                System.out.println("Formato de data inválido. Alteração ignorada.");
+            do {
+                try {
+                    System.out.print("Deseja alterar a data de publicação? (S/N): ");
+                    resposta = scanner.nextLine().toUpperCase();
+
+                    if (!resposta.equals("N") && !resposta.equals("S")) {
+                        System.out.println(" ");
+                        System.out.println(" -- Por favor, digite apenas 'S' para Sim ou 'N' para Não");
+                        System.out.println(" ");
+                        continue;
+                    }
+                    break;
+
+
+                } catch (Exception e) {
+                    System.out.println(" -- Houve um erro! Tente novamente! -- ");
+                    System.out.println(" ");
+
+                }
+            }while (true);
+
+
+            if (resposta.equalsIgnoreCase("S")) {
+                System.out.print("Nova data de publicação (dd/MM/yyyy): ");
+                String novaDataStr = scanner.nextLine();
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    Date novaDataPublicacao = sdf.parse(novaDataStr);
+                    videoParaEditar = new Video(videoParaEditar.getTitulo(), videoParaEditar.getDescricao(), videoParaEditar.getDuracao(), videoParaEditar.getCategoria(), novaDataPublicacao);
+                } catch (Exception e) {
+                    System.out.println("Formato de data inválido. Alteração ignorada.");
+                }
             }
+
+
+            videoService.deleteVideo(videoAnteriorParaDeletar);
+            videoService.addVideo(videoParaEditar);
+            System.out.println("Informações do vídeo atualizadas com sucesso!");
         }
 
 
-        videoService.deleteVideo(videoAnteriorParaDeletar);
-        videoService.addVideo(videoParaEditar);
-        System.out.println("Informações do vídeo atualizadas com sucesso!");
     }
 
     public void excluirVideo (Scanner scanner, SearchStrategy searchStrategy, VideoService videoService, String buscaVideoExclusao) {
